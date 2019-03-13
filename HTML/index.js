bc98ff5c2dbc9eb95e7a5b95ac08e19df97f82a4
Jalindar Pandharkar (OWQ8) <jalindarpandharkar@gmail.com>
2019-03-13 18:10:07 +0530

    html page

---
+++ HTML/index.js
@@ -0,0 +1,6 @@
+
+function add() {
+    var Number1 = (document.getElementById('Num1')).value;
+    const Number2 = document.getElementById('Num2').value;
+    document.getElementById('lResult').innerHTML = Number(Number1) + Number(Number2);    
+  }
